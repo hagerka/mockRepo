7b4bc969e3c9d18501091a102fe8494bbb4024b8
Kimberly Hager <hager.kimberly.a@gmail.com>
2023-07-24 22:16:12 -0400

    Lots of commits for lots of packages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * from /function1.js
-import * from /function2.js
-import * from /function4.js
-import * from /folder/function3.js
+import { function1 } from "./function1.js";
+import { function2 } from "./function2.js";
+import { function3 } from "./function3.js";
+import { function4 } from "./function4.js";
 
-export {function1, function2, function3, function4}
+export { function1, function2, function3, function4 };
